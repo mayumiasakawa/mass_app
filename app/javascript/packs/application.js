b5af117c2f862d22730dad0d9ac1967827335f7a
mayumiasakawa <m.a.mayumi214@gmail.com>
2021-02-18 22:11:27 +0900

    file move

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('./index')
+require('./sb-admin-2')
+require('./sb-admin-2.min')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
